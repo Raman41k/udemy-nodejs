a3f9cb652ebfbb4493d2d2b63fb0f9c40ee8a980
Roman <romangugel@gmail.com>
2025-02-14 17:11:41 +0200

    section 5 75 lesson

--- src/controllers/App.controller.js
+++ src/controllers/App.controller.js
@@ -1,19 +1,19 @@
 const { renderHtml } = require('../helpers/utils')
 
-const getHomePage = (req, res) => {
+const getHomePage = (req, res, next) => {
     renderHtml(res, 'shop')
 };
 
-const addProductPage = (req, res) => {
+const addProductPage = (req, res, next) => {
     renderHtml(res, 'add-product')
 };
 
-const addProduct = (req, res) => {
+const addProduct = (req, res, next) => {
     console.log(req.body)
     res.redirect('/');
 }
 
-const get404Page = (req, res) => {
+const get404Page = (req, res, next) => {
     renderHtml(res, '404')
 };
 
