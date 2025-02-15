8f834c56af9cc0772642b0d3310fd5c2554f6c50
raman41k <romangugel@gmail.com>
2025-02-15 11:50:50 +0200

    lesson 76 add static files

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express =  require('express');
+const path = require("path");
 const bodyParser = require("body-parser");
+
 
 const app = express();
 const shopRouter = require('./src/routes/Shop.routes');
 const adminRouter = require('./src/routes/Admin.routes');
+
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, './src/public')));
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
